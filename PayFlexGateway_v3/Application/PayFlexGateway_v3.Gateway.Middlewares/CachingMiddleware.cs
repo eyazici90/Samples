b6c25c375981c897252584daa79aa056730de0fb
eyazici <e.yazici1990@gmail.com>
2018-12-09 18:59:46 +0300

    multiple middlewares adds++

---
+++ PayFlexGateway_v3/Application/PayFlexGateway_v3.Gateway.Middlewares/CachingMiddleware.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayFlexGateway_v3.Gateway.Middlewares
+{
+    public class CachingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CachingMiddleware(RequestDelegate next)
+        {
+            _next = next ?? throw new ArgumentNullException(nameof(next));
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            await _next(context);
+        }
+    } 
+}
---
+++ PayFlexGateway_v3/Application/PayFlexGateway_v3.Gateway.Middlewares/CircuitBreakerMiddleware.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayFlexGateway_v3.Gateway.Middlewares
+{ 
+    public class CircuitBreakerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CircuitBreakerMiddleware(RequestDelegate next)
+        {
+            _next = next ?? throw new ArgumentNullException(nameof(next));
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            await _next(context);
+        }
+    }
+}
